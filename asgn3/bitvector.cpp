ab4f1ed7f5717ea3d9a1fb7858aea226c2c3916d
jacksonmcafee1 <jacksonmcafee1@gmail.com>
2022-07-06 16:58:20 -0400

    asgn3 completed, need to add functional main()

--- asgn3/bitvector.cpp
+++ asgn3/bitvector.cpp
@@ -1,167 +1,163 @@
-#include <cstdlib>
-#include <iostream>
-#include <vector>
-#include <cmath>
 #include "bitvector.h"
+#include <cmath>
 
-const int WORDSIZE = 32;
-const int DEFAULT_ARR = 256;
+const uint WORDSIZE = 32;
+const uint DEFAULT_ARR = 256;
 
-using namespace std;
+// set index bit
+void BitVector::Set(const size_t index) {
+  int current_word;
+  if(index > size * WORDSIZE || index < 0) {
+    // ERROR: out of range
+    std::cout << "Invalid index, out of range." << std::endl;
+    return;
+  }
+  else {
+    // find correct word then bitlocation
+    current_word = floor(index / WORDSIZE);
+    bitlocations = 32 - (index % WORDSIZE);
+  }
 
-// BitVector constructors
+  // bit setting time
+  bvect[current_word] |= 1UL << bitlocations;
+}
+
+// set all bits
+void BitVector::Set() {
+    for (int i = 0; i < size; i++) {
+      bvect[i] = 1;
+    }
+}
+
+// unset index bit
+void BitVector::Unset(const size_t index) {
+  int current_word;
+  if(index > size * WORDSIZE || index < 0) {
+    // ERROR: out of range
+    std::cout << "Invalid index, out of range." << std::endl;
+    return;
+  }
+  else {
+    // find correct word then bitlocation
+    current_word = floor(index / WORDSIZE);
+    bitlocations = 32 - (index % WORDSIZE);
+  }
+
+  // bit-unsetting time
+  bvect[current_word] &= ~(1UL << bitlocations);
+}
+
+// unset all bits
+void BitVector::Unset() {
+  for (int i = 0; i < size; i++) {
+    bvect[i] = 0;
+  }
+}
+
+// flip index bit
+void BitVector::Flip(const size_t index) {
+  int current_word;
+  if(index > size * WORDSIZE || index < 0) {
+    // ERROR: out of range
+    std::cout << "Invalid index, out of range." << std::endl;
+    return;
+  }
+  else {
+    // find correct word then bitlocation
+    current_word = floor(index / WORDSIZE);
+    bitlocations = 32 - (index % WORDSIZE);
+  }
+
+  // bit-flipping time
+  bvect[current_word] ^= 1UL << bitlocations;
+}
+
+// flip all bits
+void BitVector::Flip() {
+  for(int i = 0; i < size; i++) {
+    bvect[i] = ~bvect[i];
+  }
+}
+
+bool BitVector::Test(size_t index) const {
+  int current_word;
+  int l_bitlocations;
+  if(index > size * WORDSIZE || index < 0) {
+    // ERROR: out of range
+    std::cout << "Invalid index, out of range." << std::endl;
+    return false;
+  }
+  else {
+    // find correct word then bitlocation
+    current_word = floor(index / WORDSIZE);
+    l_bitlocations = 32 - (index % WORDSIZE);
+  }
+
+  bool bit = (bvect[current_word] >> l_bitlocations) & 1U;
+  return bit;
+}
+
+void BitVector::Print() {
+  for (int i = 0; i < size; i++) {
+    std::cout << bvect[i] << " ";
+  }
+  std::cout << std::endl;
+}
+
+// default constructor
 BitVector::BitVector() {
-    size = (DEFAULT_ARR/WORDSIZE);
-    // creates bitvec
+    size = DEFAULT_ARR/WORDSIZE;
     bvect = new int[size];
+    // defaults bvect to 0s
     for(int i = 0; i < size; i++) {
-	bvect[i] = 0;
-	}
+      bvect[i] = 0;
+      }
+    bitlocations = 0;
 }
 
+// explicit constructor
 BitVector::BitVector(size_t bv_size) {
-    // remainder logic, testing for +1 to size (# of words)
-    bool no_remainder = false;
-    if (bv_size % WORDSIZE == 0) {
-        no_remainder = true;
+    // checks if extra word needs to be allocated
+    if(bv_size % WORDSIZE == 0) {
+      size = bv_size / WORDSIZE;
+    } else {
+      size = (bv_size / WORDSIZE) + 1;
     }
-    
-    if (no_remainder == true) {
-        size = (bv_size/WORDSIZE);
-    }
-    else {
-        size = (bv_size/WORDSIZE) + 1;
-    }
-    
-    // creates bitvec
+
     bvect = new int[size];
-    for(int i = 0; i < (size); i++) {
-	bvect[i] = 0;
-	}
+    // defaults bvect to 0s
+    for(int i = 0; i < size; i++) {
+      bvect[i] = 0;
+      }
+    bitlocations = 0;
 }
 
-BitVector::BitVector( const BitVector &obj) {
-    size = obj.size;
-    bitlocations = obj.bitlocations;
+// copy constructor
+BitVector::BitVector(const BitVector &copy) {
+    size = copy.size;
+    bitlocations = copy.bitlocations;
     bvect = new int[size];
     for(int i = 0; i < size; i++) {
-	bvect[i] = obj.bvect[i];
-	}
+        bvect[i] = copy.bvect[i];
+    }
 }
 
+// destructor
 BitVector::~BitVector() {
     delete[] bvect;
 }
 
-void BitVector::Set() {
-    // sets all bit values to 1
-    // 255(11111111)
-    for(int i = 0; i < size; i++) {
-	bvect[i] = 255;
-	}
-}
+BitVector & BitVector::operator =(const BitVector &a) {
+    if (this == &a) return *this; // prevents self assignment
 
-void BitVector::Set(const size_t index) {
-    // locating bit
-    int word_index = round(floor(index/size));
-    bitlocations = 31 - (index % 32);
-    int temp = bvect[word_index];
-    // creating bitmask
-    // 1(00000001)
-    int bitmask = 1;
-
-    if(index < 0 || index > size*word_index) {
-        return; }
-    
-
-    bitmask = bitmask << bitlocations;
-    // logical OR and setting the bit
-    temp = temp | bitmask;
-    bvect[word_index] = temp;
-
-}
-
-void BitVector::Unset() {
-    // sets all bit values to 0
-    // 0(00000000)
-    for(int i = 0; i < size; i++) {
-	bvect[i] = 0;
-	}
-}
-
-void BitVector::Unset(const size_t index) {
-    // locating bit
-    int word_index = round(floor(index/size));
-    bitlocations = 31 - (index % 32);
-    // creating bitmask of 0
-    int temp1 = bvect[word_index];
-    int temp2 = bvect[word_index];
-    int bitmask1 = 0;
-    
-    if(index < 0 || index > size*word_index) {
-        return; }
-
-    // shifting bit to first pos
-    temp1 = temp1 >> bitlocations;
-    // logical AND with 0 bitmask,
-    temp1 = temp1 ^ bitmask1;
-    if(temp1 == 1) {
-	temp2 &= ~(1UL << bitlocations);
-	bvect[word_index] = temp2;
-	}
-}
-
-void BitVector::Flip() {
-    // flips every bit
-    for(int i = 0; i < size; i++) {
-    	int temp = ~bvect[i];
-    	bvect[i] = temp;
-	}
-}
-
-void BitVector::Flip(const size_t index) {
-    int word_index = round(floor(index/size));
-    bitlocations = 31 - (index % 32);
-    int temp = bvect[word_index];
-    // creating bitmask
-    int bitmask = 1;
-
-    if(index < 0 || index > size*word_index) {
-	return; }    
-
-    bitmask = bitmask << bitlocations;
-    // apply bitmask
-    temp = temp ^ bitmask;
-    bvect[word_index] = temp;
-    
-}
-
-bool BitVector::Test (size_t index) const {
-    int word_index = round(floor(index/size));
-    int bitloc = 31 - (index % 32);
-    int bittemp = bvect[word_index];
-    int bitmask = 1;
-    // shifts bit in question to first position
-    bittemp = bittemp >> bitloc;
-    // prints bit
-    bittemp = bittemp & bitmask;
-    cout << bittemp << endl;
-    return true;    
-}
-
-void BitVector::Print() {
-    // uses cout to display 
-    for(int j = 0; j < size; j++) {
-        int k = bvect[j];
-	for (int i = 31; i >= 0; i--) {
-        k = k >> i;
-        if (k & 1)
-            cout << "1";
-        else
-            cout << "0";
-    	}
-        cout << endl;
+    size = a.size;
+    bitlocations = a.bitlocations;
+    bvect = new int[size];
+    for (int i = 0; i < size; i++) {
+      bvect[i] = a.bvect[i];
     }
+
+    return *this;
 }
 
 size_t BitVector::Size() const {
@@ -169,6 +165,8 @@ size_t BitVector::Size() const {
 }
 
 int main() {
-    // main func here
-    return 0;
+
+  // write test suite
+
+  return 0;
 }
--- asgn3/bitvector.h
+++ asgn3/bitvector.h
@@ -1,10 +1,10 @@
 #ifndef BITVECTOR_H
 #define BITVECTOR_H
 #include <iostream>
-using namespace std; 
-class BitVector 
+using namespace std;
+class BitVector
 {
-public: 
+public:
    void Set (const size_t index);    // Make index bit=1
    void Set ();  // Make all bits=1
    void Unset(const size_t index);  // Make index bit=0
@@ -13,9 +13,9 @@ public:
    void Flip ();  // Flip all bits
    bool Test (size_t index) const;  // Return index bit value
    void Print();  // Prints index value
-		  // and ascii value of 
-		  // bits turned on. 
-   
+		  // and ascii value of
+		  // bits turned on.
+
    BitVector();   // Default constructor 256 bits
    explicit BitVector (size_t bv_size);  // construct a BitVector
 				 // With specified size BitVector
@@ -23,11 +23,11 @@ public:
    ~BitVector(); // Destructor
    BitVector & operator =(const BitVector &a); // Assignment Operator
    size_t Size() const;  // Return size of bitvectorprivate;
-        
- private: 
+
+ private:
    int size;  // Number of words
-   int * bvect;  // Bit Vector dynamically 
+   int * bvect;  // Bit Vector dynamically
    int bitlocations;  // Current bit
-      
-}; 
+
+};
 #endif
